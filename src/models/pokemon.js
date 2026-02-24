ac212e4487586680e04905a74c94232274bf010d
SoyMaloProgramando <ylariza@uniboyaca.edu.co>
2026-02-23 21:02:32 -0500

    avances css y js

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -1,10 +1,10 @@
 export default class Pokemon {
-    constructor (id, name, types, sprite){
+    constructor (id, name, types, sprite, abilities, stats){
         this.id = id;
         this.name = name;
         this.types = types;
         this.sprite = sprite;
-
-
+        this.abilities = abilities;
+        this.stats = stats;
     }
 }
--- src/ui/ui.js
+++ src/ui/ui.js
@@ -1,6 +1,6 @@
 export function showPokemon(pokemon){
     if (!pokemon) return;
-    document.querySelector(".pokemon-img").onclick = ( ) => showModal(pokemon);
+    document.querySelector(".pokemon-img").onclick = () => showModal(pokemon);
 
     //datos del pokemon
     document.getElementById("pokemon-img").src = pokemon.sprite;
@@ -16,20 +16,19 @@ export function showPokemon(pokemon){
         span.textContent = capitalize(t);
         typesDiv.appendChild(span);
     });
-}
-function capitalize(word) {
-    return word.charAt(0).toUpperCase() +word.slice(1);
-}
+    //estadísticas
 
-//estadísticas
-
-const statsDiv = document.getElementById("modal-stats");
-statsDiv.innerHTML = "<h3> Estadísticas </h3>";
-pokemon.stats.forEach( s => {
-
+    const statsDiv = document.getElementById("modal-stats");
+    statsDiv.innerHTML = "<h3> Estadísticas </h3>";
+    pokemon.stats.forEach( s => {
         const span = document.createElement("span");
         span.classList.add("type", t);
         span.textContent = capitalize(t);
         typesDiv.appendChild(span);
 
 });
+}
+function capitalize(word) {
+    return word.charAt(0).toUpperCase() +word.slice(1);
+}
+
